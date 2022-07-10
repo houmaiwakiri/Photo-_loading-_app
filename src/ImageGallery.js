0b17109c1ab1e4f18b13f3c637e34efb40dbe471
houmaiwakiri <mahomahoma888@gmail.com>
2022-07-10 23:27:30 +0900

    完成あとはfirebase使うか悩み中

---
+++ src/ImageGallery.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./ImageGallery.css"
+
+const ImageGallery = ({fetchData}) => {
+  return (
+    <div>
+        <div className="images-wrapper">
+            {fetchData.map((data) => (
+            <div className="image" key = {data.id} >
+                <img 
+                src={data.largeImageURL} 
+                alt="" 
+                />
+            </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default ImageGallery
